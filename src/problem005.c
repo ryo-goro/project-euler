ed3e2d9dbd5954a6bbf74c9766a7436ea9d542d3
ryo-goro <aaryosuke@gmail.com>
2025-09-09 12:18:42 +0900

    Changed the data type of sieve and renamed an argument of make_sieve for clarity

--- src/problem005.c
+++ src/problem005.c
@@ -1,43 +1,43 @@
 // Smallest Multiple
+// 232792560
 
 #include <stdio.h>
 
 #define LIMIT 20
 
-void make_sieve(int *sieve, int n)
+void make_sieve(char *sieve, int sieve_len)
 {
-    if (n <= 0) {
+    if (sieve_len <= 0) {
         return;
     }
 
-    // n > 0
+    // sieve_len > 0
     // 0 is not a prime number
     sieve[0] = 0;
 
-    if (n == 1) {
+    if (sieve_len == 1) {
         return;
     }
 
-    // n > 1
+    // sieve_len > 1
     // 1 is not a prime number
     sieve[1] = 0;
 
-    for (int i = 2; i < n; i++) {
+    for (int i = 2; i < sieve_len; i++) {
         sieve[i] = 1;
     }
     
     // All even numbers except 2 are not prime numbers
-    for (int i = 4; i < n; i += 2) {
+    for (int i = 4; i < sieve_len; i += 2) {
         sieve[i] = 0;
     }
 
-    // Find odd prime numbers < n
-    for (int i = 3; i * i < n; i += 2) {
-        if (sieve[i]) { // i is a prime number
-
-            // Mark 2 * i, 3 * i, ... as not prime
-            // Note that 2 * i, 3 * i, ..., (i - 1) * i are already marked as not prime
-            for (int j = i * i; j < n; j += i) {
+    // Find odd prime numbers < sieve_len
+    for (int i = 3; i * i < sieve_len; i += 2) {
+        // If i is a prime number, mark 2 * i, 3 * i, ... as not prime
+        // Note that 2 * i, 3 * i, ..., (i - 1) * i are already marked as not prime
+        if (sieve[i]) {
+            for (int j = i * i; j < sieve_len; j += i) {
                 sieve[j] = 0;
             }
         }
@@ -46,7 +46,7 @@ void make_sieve(int *sieve, int n)
 
 int main(void)
 {
-    int sieve[LIMIT + 1];
+    char sieve[LIMIT + 1];
     make_sieve(sieve, LIMIT + 1);
 
     long long res = 1LL;
@@ -57,6 +57,7 @@ int main(void)
             while (power * p <= LIMIT) {
                 power *= p;
             }
+
             res *= power;
         }
     }

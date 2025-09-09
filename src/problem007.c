1c858fc8e2ea18efe657d4937a74a41adf160d1e
ryo-goro <aaryosuke@gmail.com>
2025-09-09 12:26:29 +0900

    Renamed variables for clarity

--- src/problem007.c
+++ src/problem007.c
@@ -1,4 +1,5 @@
 // 10 001st Prime
+// 104743
 
 #include <stdio.h>
 
@@ -6,33 +7,33 @@
 
 int main(void)
 {
-    long prime_numbers[LIMIT + 1];
-    prime_numbers[0] = 2;
+    long primes[LIMIT + 1];
+    primes[0] = 2;
 
-    int num = 1;
+    int num_of_primes = 1;
 
-    for (long p = 3; num <= LIMIT; p += 2) {
+    for (long prime_candidate = 3; num_of_primes <= LIMIT; prime_candidate += 2) {
         int found = 1;
 
-        for (int i = 0; i < num; i++) {
-            long prime_number = prime_numbers[i];
+        for (int i = 0; i < num_of_primes; i++) {
+            long prime_number = primes[i];
 
-            if (prime_number * prime_number > p) {
+            if (prime_number * prime_number > prime_candidate) {
                 break;
             }
 
-            if (p % prime_number == 0) {
+            if (prime_candidate % prime_number == 0) {
                 found = 0;
                 break;
             }
         }
 
         if (found) {
-            prime_numbers[num++] = p;
+            primes[num_of_primes++] = prime_candidate;
         }
     }
 
-    printf("%ld\n", prime_numbers[LIMIT]);
+    printf("%ld\n", primes[LIMIT]);
 
     return 0;
 }

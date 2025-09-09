a99c9761659e3c01fc927e9e0400e9053ffe0fc2
ryo-goro <aaryosuke@gmail.com>
2025-09-09 12:46:30 +0900

    Put the part of the code that calculates the sum of multiples into its own function

--- src/problem001.c
+++ src/problem001.c
@@ -1,22 +1,36 @@
 // Multiples of 3 or 5
+// 233168
 
 #include <stdio.h>
 
 #define LIMIT 1000
 
+long sum_of_multiples(int multiple, int limit)
+{
+    if (limit <= 0) {
+        return 0;
+    }
+
+    if (multiple == 0) {
+        return 0;
+    }
+
+    if (multiple < 0) {
+        multiple = -multiple;
+    }
+
+    long n = (limit - 1) / multiple;
+
+    return n * (n + 1) / 2 * multiple;
+}
+
 int main(void)
 {
-    long num3 = (LIMIT - 1) / 3;
-    long num5 = (LIMIT - 1) / 5;
-    long num15 = (LIMIT - 1) / 15;
+    long sum3  = sum_of_multiples(3,  LIMIT);
+    long sum5  = sum_of_multiples(5,  LIMIT);
+    long sum15 = sum_of_multiples(15, LIMIT);
 
-    long sum3 = num3 * (num3 + 1) / 2 * 3;
-    long sum5 = num5 * (num5 + 1) / 2 * 5;
-    long sum15 = num15 * (num15 + 1) / 2 * 15;
-
-    long res = sum3 + sum5 - sum15;
-
-    printf("%ld\n", res);
+    printf("%ld\n", sum3 + sum5 - sum15);
 
     return 0;
 }

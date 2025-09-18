388e120ce570e2930bfc1247e3edc964dc9af577
ryo-goro <aaryosuke@gmail.com>
2025-09-19 08:05:17 +0900

    Moved some logic into separate functions, and added comments

--- src/problem012.c
+++ src/problem012.c
@@ -1,59 +1,83 @@
 // Highly Divisible Triangular Number
+// 76576500
 
 #include <stdio.h>
 
 #define THRESHOLD 500
-#define ARRAY_LEN 10000
+#define CACHE_LEN 10000
 
-// odd_num should be an odd number > 1
+// odd_num must be an odd number that is > 1
+// However, this code does not include validation in order to optimize performance
 int compute_num_of_divisors(int odd_num)
 {
     int res = 2;
 
     int d = 3;
-    for (; d * d < odd_num; d += 2)
-        if (odd_num % d == 0)
+    for (; d * d < odd_num; d += 2) {
+        if (odd_num % d == 0) {
             res += 2;
+        }
+    }
     
-    if (d * d == odd_num)
+    if (d * d == odd_num) {
         res++;
+    }
     
     return res;
 }
 
-int main(void)
+// num must be positive
+// However, this code does not include validation in order to optimize performance
+int compute_num_of_divisors_with_cache(int num)
 {
     // cache[i] = the number of positive divisors of 2i+1
-    int cache[ARRAY_LEN];
-    cache[0] = 1;
+    static int cache[CACHE_LEN] = {1};
+
+    int exponent_of_2_plus_1 = 1;
+
+    while (num % 2 == 0) {
+        num /= 2;
+        exponent_of_2_plus_1++;
+    }
+
+    // Here num is odd, so it can be represented as num = 2n+1
+    int n = num / 2;
+
+    if (n < CACHE_LEN) {
+        if (cache[n] == 0) {
+            cache[n] = compute_num_of_divisors(num);
+        }
+        return cache[n] * exponent_of_2_plus_1;
+    }
+    
+    return compute_num_of_divisors(num) * exponent_of_2_plus_1;
+}
+
+int main(void)
+{
+    // Let t(n) be the n-th triangle number, that is, t(n) = 1/2 * n * (n + 1)
+    // - t(2n - 1) = n(2n - 1)
+    // - t(2n)     = n(2n + 1)
 
     int n = 1;
-    int odd = 1;
+    int odd = 1;    // Either 2n - 1 or 2n + 1 for each repetition
 
-    while (1) {
-        int m = n;
-        int factor2 = 1;
-
-        while (m % 2 == 0) {
-            m /= 2;
-            factor2++;
-        }
-
-        int p = factor2 * cache[m / 2];
-        int num_of_divisors = p * cache[odd / 2];
+    for (;; n++) {
+        int num_of_divisors_n = compute_num_of_divisors_with_cache(n);
+        int num_of_divisors = num_of_divisors_n * compute_num_of_divisors_with_cache(odd);
 
         if (num_of_divisors > THRESHOLD) {
+            // t(2n - 1) exceeds the threshold
             break;
         }
 
         odd += 2;
-        num_of_divisors = p * (cache[odd / 2] = compute_num_of_divisors(odd));
+        num_of_divisors = num_of_divisors_n * compute_num_of_divisors_with_cache(odd);
 
         if (num_of_divisors > THRESHOLD) {
+            // t(2n) exceeds the threshold
             break;
         }
-
-        n++;
     }
 
     printf("%ld\n", ((long)n) * odd);

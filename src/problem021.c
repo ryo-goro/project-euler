0d4b276fbb826e56bace90a5c48427f72874b2f8
ryo-goro <aaryosuke@gmail.com>
2025-09-23 02:59:47 +0900

    Implemented a cache feature in sum_of_proper_divisors

--- src/problem021.c
+++ src/problem021.c
@@ -1,18 +1,21 @@
 // Amicable Numbers
-// Cache can be used
+// 31626
 
 #include <stdio.h>
 
 #define LIMIT 10000
 
+// A CACHE_LEN of 10000 results in 8156 hits per 19994 requests (the number of requests made by the for loop in the main function)
+// A CACHE_LEN of 100000 results in a similar number of hits (8220) per 19994 requests 
+// So a CACHE_LEN of 10000 is enough
+#define CACHE_LEN LIMIT
+
+// n should be > 1
 int sum_of_proper_divisors(int n)
 {
-    if (n <= 1) {
-        return 0;
-    }
-
     int sum = 1;
     int d;
+
     for (d = 2; d * d < n; d++) {
         if (n % d == 0) {
             sum += d + n / d;
@@ -26,18 +29,36 @@ int sum_of_proper_divisors(int n)
     return sum;
 }
 
+// n should be > 1
+int sum_of_proper_divisors_with_cache(int n)
+{
+    static int cache[CACHE_LEN] = {0};
+
+    if (n < CACHE_LEN && cache[n] > 0) {
+        return cache[n];
+    }
+
+    int res = sum_of_proper_divisors(n);
+
+    if (n < CACHE_LEN) {
+        cache[n] = res;
+    }
+
+    return res;
+}
+
 int main(void)
 {
     int res = 0;
 
     for (int a = 1; a < LIMIT; a++) {
-        int b = sum_of_proper_divisors(a);
+        int b = sum_of_proper_divisors_with_cache(a);
 
         if (a == b) {
             continue;
         }
         
-        if (a == sum_of_proper_divisors(b)) {
+        if (a == sum_of_proper_divisors_with_cache(b)) {
             res += a;
         }
     }

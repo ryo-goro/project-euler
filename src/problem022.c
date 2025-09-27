771c16b1c28f70ab9d3cce81880c9d0945438f24
ryo-goro <aaryosuke@gmail.com>
2025-09-27 16:38:24 +0900

    Moved some logic into separate functions for readability

--- src/problem022.c
+++ src/problem022.c
@@ -1,82 +1,115 @@
 // Names Scores
+// 871198282
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+long count_char(FILE *fp, int target_ch)
+{
+    fseek(fp, 0, SEEK_SET);
+
+    long count = 0;
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == target_ch) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+long count_all_chars(FILE *fp)
+{
+    fseek(fp, 0, SEEK_SET);
+
+    long count = 0;
+
+    while (fgetc(fp) != EOF) {
+        count++;
+    }
+
+    return count;
+}
+
+void load_names(FILE *fp, char *dst)
+{
+    fseek(fp, 0, SEEK_SET);
+
+    long idx = 0;
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '"') {
+            continue;
+        }
+        dst[idx++] = (ch == ',') ? '\0' : ch;
+    }
+
+    dst[idx] = '\0';
+}
 
 int compare(const char **s, const char **t)
 {
     return strcmp(*s, *t);
 }
 
+int name_score(const char *name)
+{
+    int score = 0;
+
+    while (*name) {
+        score += *name++ - 'A' + 1;
+    }
+
+    return score;
+}
+
 int main(void)
 {
-    FILE *fp;
-    if ((fp = fopen("../text/problem022.txt", "r")) == NULL) {
-        fprintf(stderr, "Cannot open ../text/problem022.txt\n");
+    const char *text_path = "../text/problem022.txt";
+    FILE *fp = fopen(text_path, "r");
+
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open %s\n", text_path);
         return 1;
     }
 
-    int ch;
-    int raw_line_len = 0;
-    int names_count = 1;
+    long num_of_quots = count_char(fp, '"');
+    long raw_line_len = count_all_chars(fp) - num_of_quots + 1;
 
-    while ((ch = fgetc(fp)) != EOF) {
-        if (ch == '"') {
-            continue;
-        }
-        if (ch == ',') {
-            names_count++;
-        }
-        raw_line_len++;
-    }
-
-    char *raw_line = (char *)malloc(raw_line_len + 1);
-    if (raw_line == NULL) {
-        fprintf(stderr, "Out of memory\n");
-        fclose(fp);
-        return 1;
-    }
-
-    char **names = (char **)calloc(names_count, sizeof(char *));
-    if (names == NULL) {
-        fprintf(stderr, "Out of memory\n");
-        fclose(fp);
-        free(raw_line);
-        return 1;
-    }
+    char *raw_line = (char *)malloc(raw_line_len);
+    load_names(fp, raw_line);
     
-    names[0] = &raw_line[0];
-    names_count = 1;
-
-    fseek(fp, 0, SEEK_SET);
-    
-    int idx = 0;
-    while ((ch = fgetc(fp)) != EOF) {
-        if (ch == '"') {
-            continue;
-        }
-        if (ch == ',') {
-            raw_line[idx++] = '\0';
-            names[names_count++] = &raw_line[idx];
-        } else {
-            raw_line[idx++] = ch;
-        }
-    }
-    raw_line[raw_line_len] = '\0';
-
     fclose(fp);
 
-    qsort(names, names_count, sizeof(char *), (int (*)(const void *, const void *))compare);
+    long num_of_names = num_of_quots / 2;
+    char **names = (char **)calloc(num_of_names, sizeof(char *));
+    
+    long name_count = 0;
+    char *raw_line_ptr = raw_line;
+    int flag = 1;
+
+    while (name_count < num_of_names) {
+        if (flag) {
+            names[name_count++] = raw_line_ptr;
+            flag = 0;
+        }
+
+        if (*raw_line_ptr == '\0') {
+            flag = 1;
+        }
+
+        raw_line_ptr++;
+    }
+
+    qsort(names, num_of_names, sizeof(char *), (int (*)(const void *, const void *))compare);
 
     long total = 0;
-    for (int i = 0; i < names_count; i++) {
-        char *name = names[i];
-        long score = 0;
-        while (*name) {
-            score += *name++ - 'A' + 1;
-        }
-        total += score * (i + 1);
+    for (int i = 0; i < num_of_names; i++) {
+        total += ((long)name_score(names[i])) * (i + 1);
     }
     
     printf("%ld\n", total);

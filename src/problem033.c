1c3f043f50261f59ebdc00e6d02a3c475d2d7948
ryo-goro <aaryosuke@gmail.com>
2025-10-07 17:51:08 +0900

    Stylistic changes

--- src/problem033.c
+++ src/problem033.c
@@ -1,13 +1,12 @@
 // Digit Cancelling Fractions
+// 100
 
 #include <stdio.h>
 
 long gcd(long a, long b)
 {
-    long r;
-
     while (b > 0L) {
-        r = a % b;
+        long r = a % b;
         a = b;
         b = r;
     }
@@ -17,36 +16,40 @@ long gcd(long a, long b)
 
 int main(void)
 {
-    long numerators = 1L;
-    long denominators = 1L;
+    long numerator = 1;
+    long denominator = 1;
     
     // The target fraction is a / b where 10 <= a < b < 100
     // Let a = 10a2 + a1, b = 10b2 + b1 (a1, b1 != 0)
     // Then the possibilities are:
-    // - a1 == b2, and a / b = (10a2 + a1) / (10a1 + b1) = a2 / b1
-    // - a2 == b1, and a / b = (10a2 + a1) / (10b2 + a2) = a1 / b2
+    // - a1 = b2, and a / b = (10a2 + a1) / (10a1 + b1) = a2 / b1
+    // - a2 = b1, and a / b = (10a2 + a1) / (10b2 + a2) = a1 / b2
     for (int a = 10; a < 100; a++) {
         int a1 = a % 10;
+
         if (a1 == 0) {
             continue;
         }
+
         int a2 = a / 10;
 
         for (int b = a + 1; b < 100; b++) {
             int b1 = b % 10;
+
             if (b1 == 0) {
                 continue;
             }
+
             int b2 = b / 10;
 
             if ((a1 == b2 && a * b1 == b * a2) || (a2 == b1 && a * b2 == b * a1)) {
-                numerators *= a;
-                denominators *= b;
+                numerator *= a;
+                denominator *= b;
             }
         }
     }
 
-    printf("%ld\n", denominators / gcd(denominators, numerators));
+    printf("%ld\n", denominator / gcd(denominator, numerator));
 
     return 0;
 }

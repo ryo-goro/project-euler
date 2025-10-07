c3469c9e2fdbe011b075bf2417e78cfd0b15244e
ryo-goro <aaryosuke@gmail.com>
2025-10-07 16:32:03 +0900

    Stylistic changes

--- src/problem030.c
+++ src/problem030.c
@@ -1,4 +1,5 @@
 // Digit Fifth Powers
+// 443839
 
 #include <stdio.h>
 
@@ -10,20 +11,22 @@ int main(void)
         power5[i] = p * p * p * p * p;
     }
 
-    long total = 0L;
+    long total = 0;
 
     // Let x an n-digit integer which can be written as the sum of fifth powers of its digits
     // Then 10^(n-1) <= x <= n * 9^5 = 59049n
-    // Only n = 1, 2, 3, 4, 5, 6 hold this condition
-    for (long i = 2L; i < 1000000L /* 10^6 */; i++) {
-        long p = i;
-        long sum = 0L;
+    // Only n = 1, 2, 3, 4, 5 and 6 hold this condition
+    for (long target = 2; target < 1000000L /* 10^6 */; target++) {
+        long p = target;
+        long sum = 0;
+
         while (p > 0L) {
-            sum += power5[p % 10L];
-            p /= 10L;
+            sum += power5[p % 10];
+            p /= 10;
         }
-        if (sum == i) {
-            total += i;
+
+        if (sum == target) {
+            total += target;
         }
     }
 

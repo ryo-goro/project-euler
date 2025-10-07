7bc46b1d055a74dffeeab98cdef4501d7606c7c4
ryo-goro <aaryosuke@gmail.com>
2025-10-07 16:43:31 +0900

    Changed the names of some variables and added a comment

--- src/problem031.c
+++ src/problem031.c
@@ -1,10 +1,10 @@
 // Coin Sums
-// DP
+// 73682
 
 #include <stdio.h>
 
 #define NUM_OF_COINS 8
-#define PRICE 200
+#define TARGET_AMOUNT 200
 
 int main(void)
 {
@@ -12,16 +12,19 @@ int main(void)
         1, 2, 5, 10, 20, 50, 100, 200,
     };
 
-    long dp[PRICE + 1] = {1L};
+    long dp[TARGET_AMOUNT + 1] = {1};
 
     for (int i = 0; i < NUM_OF_COINS; i++) {
         int coin = coins[i];
-        for (int j = coin; j <= PRICE; j++) {
-            dp[j] += dp[j - coin];
+
+        for (int amount = coin; amount <= TARGET_AMOUNT; amount++) {
+            dp[amount] += dp[amount - coin];
         }
+
+        // Here, dp[amount] means "how many different ways `amount` can be made using coins[0], coins[1], ..., coins[i]"
     }
 
-    printf("%ld\n", dp[PRICE]);
+    printf("%ld\n", dp[TARGET_AMOUNT]);
 
     return 0;
 }

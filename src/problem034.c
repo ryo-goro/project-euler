a845c9a011b28ae8d3c79dba5efed86cfbac892d
ryo-goro <aaryosuke@gmail.com>
2025-10-07 17:58:42 +0900

    Stylistic changes

--- src/problem034.c
+++ src/problem034.c
@@ -1,27 +1,28 @@
 // Digit Factorials
+// 40730
 
 #include <stdio.h>
 
 int main(void)
 {
-    long factorial[10] = {1L};
+    long factorial[10] = {1};
     for (int i = 1; i < 10; i++) {
         factorial[i] = factorial[i - 1] * i;
     }
 
-    long long total = 0LL;
+    long long total = 0;
 
     // Let x an n-digit integer (n >= 2) which can be written as the sum of the factorial of its digits
     // Then 10^(n-1) <= x <= n * 9! = 362880n
     // Only n = 2, 3, 4, 5, 6, 7 hold this condition
     // So 10 <= x <= 362880 * 7 = 2540160
-    for (long x = 10L, limit = factorial[9] * 7; x <= limit; x++) {
-        long sum = 0L;
+    for (long x = 10, limit = factorial[9] * 7; x <= limit; x++) {
+        long sum = 0;
         long y = x;
 
         do {
-            sum += factorial[y % 10L];
-            y /= 10L;
+            sum += factorial[y % 10];
+            y /= 10;
         } while (y > 0L);
         
         if (sum == x) {

a5e336d93850102a3a1be7703a016f5e09e6e297
ryo-goro <aaryosuke@gmail.com>
2025-10-11 17:56:07 +0900

    Renamed a function to make its purpose clearer

--- src/problem040.c
+++ src/problem040.c
@@ -1,38 +1,49 @@
 // Champernowne's Constant
+// 210
 
 #include <stdio.h>
 
-// Returns the n-th digit of the fractional part of the Champernowne constant
-int get_digit(long n)
+// Returns a^n
+long power(long a, int n)
 {
-    long power10 = 1L;
-    long s = 1L;    // 9 * (1 * 10^0 + 2 * 10^1 + 3 * 10^2 + ...)
+    long res = 1;
+
+    for (int i = 0; i < n; i++) {
+        res *= a;
+    }
+
+    return res;
+}
+
+// Returns the n-th digit of the fractional part of the Champernowne constant
+int champernowne_digit(long n)
+{
+    long power10 = 1;
+    long s = 1; // 9 * (1 * 10^0 + 2 * 10^1 + 3 * 10^2 + ...)
     long increment;
     int num_of_digits = 1;
 
     while (s + (increment = 9 * num_of_digits * power10) <= n) {
         s += increment;
         num_of_digits++;
-        power10 *= 10L;
+        power10 *= 10;
     }
 
     long target = power10 + (n - s) / num_of_digits;
 
     for (int i = 1; i < num_of_digits - (int)((n - s) % num_of_digits); i++) {
-        target /= 10L;
+        target /= 10;
     }
 
-    return (int)(target % 10L);
+    return (int)(target % 10);
 }
 
 int main(void)
 {
-    long power10 = 1L;
-    long res = 1L;
+    long res = 1;
 
-    for (int i = 0; i < 7; i++) {
-        res *= get_digit(power10);
-        power10 *= 10L;
+    for (int n = 0; n < 7; n++) {
+        res *= champernowne_digit(power(10, n));
     }
     
     printf("%ld\n", res);

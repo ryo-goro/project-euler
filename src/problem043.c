81034dcbba2d0214c4d991556b7e0b1367537e66
ryo-goro <aaryosuke@gmail.com>
2025-10-14 17:10:26 +0900

    Introduced a function to make the main function shorter

--- src/problem043.c
+++ src/problem043.c
@@ -1,48 +1,65 @@
 // Sub-string Divisibility
+// 16695334890
 
 #include <stdio.h>
 
 #define swap(type, x, y)    do { type t = x; x = y; y = t; } while (0)
 
-void reverse(int *a, int l, int r)
+void reverse_arr(int *arr, int begin, int end)
 {
-    for (int i = 0, h = (r - l) / 2; i < h; i++) {
-        swap(int, a[l + i], a[r - 1 - i]);
+    for (int i = 0, h = (end - begin) / 2; i < h; i++) {
+        int tmp = arr[begin + i];
+        arr[begin + i] = arr[end - 1 - i];
+        arr[end - 1 - i] = tmp;
     }
 }
 
-int next_permutation(int *a, int n)
+int next_permutation(int *perm, int perm_len)
 {
-    int i = n - 2;
+    int i = perm_len - 2;
     for (; i >= 0; i--) {
-        if (a[i] < a[i + 1]) {
+        if (perm[i] < perm[i + 1]) {
             break;
         }
     }
 
     if (i < 0) {
-        reverse(a, 0, n);
+        reverse_arr(perm, 0, perm_len);
         return 0;
     }
 
-    int tmp = a[i];
-    int j = n - 1;
+    int tmp = perm[i];
+    int j = perm_len - 1;
     for (; j > i; j--) {
-        if (a[j] > tmp) {
+        if (perm[j] > tmp) {
             break;
         }
     }
 
-    swap(int, a[i], a[j]);
-    reverse(a, i + 1, n);
+    swap(int, perm[i], perm[j]);
+    reverse_arr(perm, i + 1, perm_len);
 
     return 1;
 }
 
-long to_ll(const int *digits, int n)
+// Example: to_ll_rev({9, 6, 7, 8}, 3) = 967
+long long to_ll_rev(const int *digits, int n)
 {
-    long long res = digits[0];
-    for (int i = 1; i < n; i++) {
+    long long res = 0;
+
+    for (int i = 0; i < n; i++) {
+        res = res * 10 + digits[i];
+    }
+
+    return res;
+}
+
+// Example: to_int_rev({9, 6, 7, 8, 9}, 1, 3) = 67
+int to_int_rev(const int *digits, int begin, int end)
+{
+    int res = 0;
+
+    for (int i = begin; i < end; i++) {
         res = res * 10 + digits[i];
     }
 
@@ -51,45 +68,27 @@ long to_ll(const int *digits, int n)
 
 int main(void)
 {
-    int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    long long res = 0LL;
+    int primes[] = {2, 3, 5, 7, 11, 13, 17};
+    int num_of_primes = sizeof(primes) / sizeof(primes[0]);
+
+    int digits[] = {1, 0, 2, 3, 4, 5, 6, 7, 8, 9};  // Skip numbers with a leading 0
+    int num_of_digits = sizeof(digits) / sizeof(digits[0]);
+
+    long long res = 0;
 
     do {
-        if (digits[0] == 0) {
-            continue;
+        int i = 0;
+
+        for (; i < num_of_primes; i++) {
+            if (to_int_rev(digits, i + 1, i + 4) % primes[i] != 0) {
+                break;
+            }
         }
 
-        if (digits[3] % 2 != 0) {
-            continue;
+        if (i == num_of_primes) {
+            res += to_ll_rev(digits, num_of_digits);
         }
-
-        if ((digits[2] + digits[3] + digits[4]) % 3 != 0) {
-            continue;
-        }
-
-        if (digits[5] != 0 && digits[5] != 5) {
-            continue;
-        }
-
-        if ((100 * digits[4] + 10 * digits[5] + digits[6]) % 7 != 0) {
-            continue;
-        }
-
-        if ((100 * digits[5] + 10 * digits[6] + digits[7]) % 11 != 0) {
-            continue;
-        }
-
-        if ((100 * digits[6] + 10 * digits[7] + digits[8]) % 13 != 0) {
-            continue;
-        }
-
-        if ((100 * digits[7] + 10 * digits[8] + digits[9]) % 17 != 0) {
-            continue;
-        }
-
-        res += to_ll(digits, 10);
-    
-    } while (next_permutation(digits, 10));
+    } while (next_permutation(digits, num_of_digits));
 
     printf("%lld\n", res);
 

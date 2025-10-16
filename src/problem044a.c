422f4f565c3a6264ce152bad52bc43de96a13628
ryo-goro <aaryosuke@gmail.com>
2025-10-17 00:15:13 +0900

    Wrote a faster version of problem044.c without the minimality verification logic

---
+++ src/problem044a.c
@@ -0,0 +1,72 @@
+// Pentagon Numbers
+// 5482660
+// p_2167 = 7042750, p_1020 = 1560090
+// p_2167 - p_1020 = 5482660 = p_1912, p_2167 + p_1020 = 8602840 = p_2395
+
+// This code is faster because it does not verify the minimality of the answer
+
+#include <stdio.h>
+
+// Returns n * (3 * n - 1) / 2, the n-th pentagonal number
+long long pentagonal_number(long long n)
+{
+    return n * (3 * n - 1) / 2;
+}
+
+// Returns the integer part of the square root of n
+unsigned long long truncated_sqrt(unsigned long long n)
+{
+    unsigned long long l = 0;
+    unsigned long long r = 1LLU << 32;
+
+    do {
+        unsigned long long m = (l + r) / 2;
+        if (n < m * m) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    } while (l < r);
+
+    return l - 1;
+}
+
+// Note that this function does not consider 0 to be pentagonal
+int is_pentagonal(unsigned long long target)
+{
+    // If target > 0 is pentagonal, there exists an integer n > 0 that fulfills target = n * (3 * n - 1) / 2
+    // Solving this quadratic equation for n yields n = (1 + sqrt(1 + 24 * target)) / 6
+
+    unsigned long long radicand = 1 + 24 * target;
+    unsigned long long root = truncated_sqrt(radicand);
+
+    return root * root == radicand && (1 + root) % 6 == 0LLU;
+}
+
+int main(void)
+{
+    long long res_diff = -1;
+    
+    for (int i = 2; ; i++) {
+        long long p_i = pentagonal_number(i);
+
+        for (int j = i - 1; j > 0; j--) {
+            long long p_j = pentagonal_number(j);
+            long long diff = p_i - p_j;
+            long long sum = p_i + p_j;
+
+            if (is_pentagonal(diff) && is_pentagonal(sum)) {
+                res_diff = diff;
+                break;
+            }
+        }
+
+        if (res_diff > 0LL) {
+            break;
+        }
+    }
+
+    printf("%lld\n", res_diff);
+
+    return 0;
+}

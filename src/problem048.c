545ed44f147eba1baf24d4a4727c13859f93899f
ryo-goro <aaryosuke@gmail.com>
2025-10-17 10:58:05 +0900

    Renamed variables and functions for clarity

--- src/problem048.c
+++ src/problem048.c
@@ -1,61 +1,69 @@
 // Self Powers
+// 9110846700
 
 #include <stdio.h>
 
 #define LIMIT 1000
 #define NUM_OF_DIGITS 10
 
-void clear(int *a, int n)
+void multiply_arr(int *arr, int n, int multiplier)
 {
     for (int i = 0; i < n; i++) {
-        a[i] = 0;
+        arr[i] *= multiplier;
     }
 }
 
-void multiply(int *a, int b, int n)
+void add_arr(int *dst, const int *src, int n)
 {
     for (int i = 0; i < n; i++) {
-        a[i] *= b;
-    }
-}
-
-void add(int *dst, const int *src, int src_len)
-{
-    for (int i = 0; i < src_len; i++) {
         dst[i] += src[i];
     }
 }
 
 void carry(int *digits, int len)
 {
-    for (int i = 1; i < len; i++) {
-        digits[i] += digits[i - 1] / 10;
-        digits[i - 1] %= 10;
+    for (int i = 0; i < len - 1; i++) {
+        digits[i + 1] += digits[i] / 10;
+        digits[i] %= 10;
     }
     digits[len - 1] %= 10;
 }
 
+void reverse_arr(int *arr, int n)
+{
+    for (int i = 0, h = n / 2; i < h; i++) {
+        int tmp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = tmp;
+    }
+}
+
+void println_arr(const int *arr, int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+    }
+    putchar('\n');
+}
+
 int main(void)
 {
-    int buf[NUM_OF_DIGITS] = {0};
-    int res[NUM_OF_DIGITS] = {0};
+    int sum[NUM_OF_DIGITS] = {0};   // The last ten digits of 1^1 + 2^2 + 3^3 + ... + 1000^1000
 
     for (int i = 1; i <= LIMIT; i++) {
-        clear(buf, NUM_OF_DIGITS);
-        buf[0] = 1;
+        int pow[NUM_OF_DIGITS] = {1};   // The last ten digits of i^i
         
         for (int j = 0; j < i; j++) {
-            multiply(buf, i, NUM_OF_DIGITS);
-            carry(buf, NUM_OF_DIGITS);
+            multiply_arr(pow, NUM_OF_DIGITS, i);
+            carry(pow, NUM_OF_DIGITS);
         }
 
-        add(res, buf, NUM_OF_DIGITS);
-        carry(res, NUM_OF_DIGITS);
+        add_arr(sum, pow, NUM_OF_DIGITS);
+        carry(sum, NUM_OF_DIGITS);
     }
 
-    for (int i = NUM_OF_DIGITS - 1; i >= 0; i--)
-        printf("%d", res[i]);
-    putchar('\n');
+    reverse_arr(sum, NUM_OF_DIGITS);
+    println_arr(sum, NUM_OF_DIGITS);
 
     return 0;
 }

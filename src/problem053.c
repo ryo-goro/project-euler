e80a507ec05d5de3bb3e647b669f8b40fac22650
ryo-goro <aaryosuke@gmail.com>
2025-10-22 13:16:44 +0900

    Modified comments

--- src/problem053.c
+++ src/problem053.c
@@ -1,4 +1,5 @@
 // Combinatoric Selections
+// 4075
 
 #include <stdio.h>
 
@@ -7,19 +8,17 @@
 
 int main(void)
 {
-    // n_C_0 = 1 (n >= 0)
-    // n_C_r = n_C_(n - r) (0 <= r <= n)
-    // n_C_r = (n - 1)_C_(r - 1) + (n - 1)_C_r (1 <= r < n)
-    long combinations[N + 1] = {1};
+    // n_C_0 = n_C_n = 1
+    // n_C_r = {n - 1}_C_{r - 1} + {n - 1}_C_r (1 <= r < n)
+    long combinations[N + 1];
 
     int count = 0;
 
     for (int n = 1; n <= N; n++) {
-        long prev = 1;
-        long curr;
+        long prev = 1;  // n_C_{r - 1}
 
         for (int r = 1; r < n; r++) {
-            curr = combinations[r - 1] + combinations[r];
+            long curr = combinations[r - 1] + combinations[r];  // n_C_r
             if (curr > THRESHOLD) {
                 count++;
                 curr = THRESHOLD + 1;

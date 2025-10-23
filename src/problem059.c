e30913ba6c12993ffb11467ff342c2b7dbf024ed
ryo-goro <aaryosuke@gmail.com>
2025-10-23 14:05:23 +0900

    Removed the commented-out code and the code handling out-of-memory error

--- src/problem059.c
+++ src/problem059.c
@@ -1,4 +1,5 @@
 // XOR Decryption
+// 129448
 
 // The password is "exp"
 // The decrypted text is in ../text/problem059_decrypted.txt
@@ -49,24 +50,13 @@ int main(void)
     }
 
     int n = count_comma(fp) + 1;
-
     char *encrypted_text = malloc(n);
-    if (encrypted_text == NULL) {
-        fprintf(stderr, "Out of memory\n");
-        fclose(fp);
-        return 1;
-    }
 
     fseek(fp, 0, SEEK_SET);
     read_chars(fp, encrypted_text);
+    fclose(fp);
 
     char *decrypted_text = malloc(n);
-    if (decrypted_text == NULL) {
-        fprintf(stderr, "Out of memory\n");
-        free(encrypted_text);
-        fclose(fp);
-        return 1;
-    }
 
     char password[PASSWORD_LEN] = "exp";
 
@@ -85,49 +75,8 @@ int main(void)
 
     printf("%ld\n", sum);
 
-    /*--- 6 candidates can be obtained by executing the code below ---*/
-
-    // int count = 0;
-
-    // for (char a = 'a'; a <= 'z'; a++) {
-    //     for (int i = 0; i < n; i += 3) {
-    //         decrypted_text[i] = encrypted_text[i] ^ a;
-    //     }
-
-    //     for (char b = 'a'; b <= 'z'; b++) {
-    //         for (int i = 1; i < n; i += 3) {
-    //             decrypted_text[i] = encrypted_text[i] ^ b;
-    //         }
-
-    //         for (char c = 'a'; c <= 'z'; c++) {
-    //             for (int i = 2; i < n; i += 3) {
-    //                 decrypted_text[i] = encrypted_text[i] ^ c;
-    //             }
-
-    //             for (int i = 0; i < n - 3; i++) {
-    //                 // The text must contain "The " or "the "
-    //                 if (
-    //                     (decrypted_text[i] == 't' || decrypted_text[i] == 'T')
-    //                     && decrypted_text[i + 1] == 'h'
-    //                     && decrypted_text[i + 2] == 'e'
-    //                     && decrypted_text[i + 3] == ' '
-    //                 ) {
-    //                     count++;
-    //                     printf("[%d] The decrypted text for the candidate password: \"%c%c%c\":\n", count, a, b, c);
-    //                     for (int j = 0; j < n; j++) {
-    //                         putchar(decrypted_text[j]);
-    //                     }
-    //                     printf("\n\n");
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
     free(decrypted_text);
     free(encrypted_text);
-    fclose(fp);
 
     return 0;
 }

b45869bb03f86d27c2311cb27a2a38d5a4120ed4
ryo-goro <aaryosuke@gmail.com>
2025-10-23 11:32:00 +0900

    Renamed functions and added comments

--- src/problem056.c
+++ src/problem056.c
@@ -1,44 +1,45 @@
 // Powerful Digit Sum
+// 972
+// a = 99, b = 95
 
 #include <stdio.h>
 
 #define LIMIT 100
 #define MAX_NUM_OF_DIGITS 256
 
-void multiply(int *digits, int num_of_digits, int multiplier)
+void multiply_arr(int *arr, int n, int multiplier)
 {
-    for (int i = 0; i < num_of_digits; i++) {
-        digits[i] *= multiplier;
+    for (int i = 0; i < n; i++) {
+        arr[i] *= multiplier;
     }
 }
 
-int carry(int *digits, int num_of_digits)
+int carry_digits(int *digits, int len)
 {
-    for (int i = 0; i < num_of_digits - 1; i++) {
+    if (len <= 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < len - 1; i++) {
         digits[i + 1] += digits[i] / 10;
         digits[i] %= 10;
     }
 
-    int top = digits[num_of_digits - 1];
+    int head = len - 1;
 
-    if (top < 10) {
-        return num_of_digits;
+    while (digits[head] >= 10) {
+        digits[head + 1] = digits[head] / 10;
+        digits[head] %= 10;
+        head++;
     }
 
-    do {
-        digits[num_of_digits++ - 1] = top % 10;
-        top /= 10;
-    } while (top >= 10);
-
-    digits[num_of_digits - 1] = top;
-
-    return num_of_digits;
+    return head + 1;
 }
 
 int sumof(const int *arr, int n)
 {
     int res = 0;
-
+    
     for (int i = 0; i < n; i++) {
         res += arr[i];
     }
@@ -49,15 +50,16 @@ int sumof(const int *arr, int n)
 int main(void)
 {
     int digits[MAX_NUM_OF_DIGITS];
-    int res = 1;
+    int res = 1;    // 1^b = 1
 
     for (int a = 2; a < LIMIT; a++) {
+        //  a^0 = 1
         int num_of_digits = 1;
         digits[0] = 1;
 
         for (int b = 1; b < LIMIT; b++) {
-            multiply(digits, num_of_digits, a);
-            num_of_digits = carry(digits, num_of_digits);
+            multiply_arr(digits, num_of_digits, a);
+            num_of_digits = carry_digits(digits, num_of_digits);
 
             int sum = sumof(digits, num_of_digits);
 

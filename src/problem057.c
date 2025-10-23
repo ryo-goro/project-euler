570f7e1fb0a0aad0aff343fb451000089424a160
ryo-goro <aaryosuke@gmail.com>
2025-10-23 12:13:43 +0900

    Renamed functions

--- src/problem057.c
+++ src/problem057.c
@@ -1,4 +1,5 @@
 // Square Root Convergents
+// 153
 
 // Let us define a sequence {a_n} by the following recurrence relation:
 //
@@ -13,7 +14,7 @@
 // - p_(n + 1) = p_n + q_n (n >= 0)
 // - q_(n + 1) = 2 * p_n + q_n (= p_n + p_(n + 1)) (n >= 0)
 // 
-// Then the followings hold:
+// Then it's easy to prove that the followings hold:
 // 
 // 1. p_n and q_n are coprime (n >= 0)
 // 2. a_n = q_n / p_n (n >= 0)
@@ -25,34 +26,33 @@
 
 #define swap(type, x, y)    do { type t = x; x = y; y = t; } while (0)
 
-void add(int *dst, const int *src, int src_len)
+void add_arr(int *dst, const int *src, int n)
 {
-    for (int i = 0; i < src_len; i++) {
+    for (int i = 0; i < n; i++) {
         dst[i] += src[i];
     }
 }
 
-int carry(int *digits, int num_of_digits)
+int carry_digits(int *digits, int len)
 {
-    for (int i = 0; i < num_of_digits - 1; i++) {
+    if (len <= 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < len - 1; i++) {
         digits[i + 1] += digits[i] / 10;
         digits[i] %= 10;
     }
 
-    int top = digits[num_of_digits - 1];
+    int head = len - 1;
 
-    if (top < 10) {
-        return num_of_digits;
+    while (digits[head] >= 10) {
+        digits[head + 1] = digits[head] / 10;
+        digits[head] %= 10;
+        head++;
     }
 
-    do {
-        digits[num_of_digits++ - 1] = top % 10;
-        top /= 10;
-    } while (top >= 10);
-
-    digits[num_of_digits - 1] = top;
-
-    return num_of_digits;
+    return head + 1;
 }
 
 int main(void)
@@ -68,15 +68,15 @@ int main(void)
 
     int count = 0;
 
-    for (int rep = 0; rep < MAX_ITERATIONS; rep++) {
+    for (int rep = 1; rep <= MAX_ITERATIONS; rep++) {
         swap(int *, denominator, numerator);
         swap(int, denominator_len, numerator_len);
 
-        add(denominator, numerator, numerator_len);
-        denominator_len = carry(denominator, denominator_len);
+        add_arr(denominator, numerator, numerator_len);
+        denominator_len = carry_digits(denominator, denominator_len);
 
-        add(numerator, denominator, denominator_len);
-        numerator_len = carry(numerator, denominator_len);
+        add_arr(numerator, denominator, denominator_len);
+        numerator_len = carry_digits(numerator, denominator_len);
 
         if (numerator_len > denominator_len) {
             count++;

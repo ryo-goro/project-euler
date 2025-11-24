0ae81747e992dc0d531f26e9eb9099d2feaeeb41
ryo-goro <aaryosuke@gmail.com>
2025-11-24 23:11:34 +0900

    Completely changed the code using backtracking method

--- src/problem061.c
+++ src/problem061.c
@@ -1,194 +1,275 @@
 // Cyclical Figurate Numbers
-// 8256(tri) + 5625(hep) + 2512(hex) + 1281(squ) + 8128(oct) + 2882(pen) = 28684
+// 28684
+// 8256(3) + 5625(4) + 2512(7) + 1281(8) + 8128(6) + 2882(5)
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define N 6
-#define LEN 100
-#define BEGIN 1000
-#define END 10000
+#define NUM_OF_POLYGONAL_TYPES 6
+#define NUM_OF_DIGITS 4
+#define SUFFIX_LEN 2
+#define INITIAL_ARRAYLIST_CAPACITY 256
 
 #define swap(type, x, y)    do { type t = x; x = y; y = t; } while (0)
 
-void reverse(int *a, int l, int r)
+typedef struct {
+    long *array;
+    int size;
+    int capacity;
+} LongArrayList;
+
+int LAL_initialize(LongArrayList *list, int initial_capacity)
 {
-    for (int i = 0, h = (r - l) / 2; i < h; i++) {
-        swap(int, a[l + i], a[r - 1 - i]);
+    list->size = 0;
+
+    if (initial_capacity > 0) {
+        if ((list->array = (long *)calloc(initial_capacity, sizeof(long))) == NULL) {
+            // Out of memory
+            list->capacity = 0;
+            return 1;
+        }
+        list->capacity = initial_capacity;
+    } else {
+        list->array = NULL;
+        list->capacity = 0;
+    }
+
+    return 0;
+}
+
+int LAL_add(LongArrayList *list, long value)
+{
+    if (list->size >= list->capacity) {
+        long *tmp_arr = (long *)realloc(list->array, list->capacity * 2 * sizeof(long));
+        if (tmp_arr == NULL) {
+            // Out of memory
+            return 1;
+        }
+        list->array = tmp_arr;
+        list->capacity *= 2;
+    }
+
+    list->array[list->size++] = value;
+    return 0;
+}
+
+// `idx` should be `0 <= idx < list->size`
+long LAL_get(const LongArrayList *list, int idx)
+{
+    return list->array[idx];
+}
+
+int LAL_size(const LongArrayList *list)
+{
+    return list->size;
+}
+
+void LAL_free(LongArrayList *list)
+{
+    free(list->array);
+    list->array = NULL;
+    list->size = list->capacity = 0;
+}
+
+int LAL_lower_bound(const LongArrayList *list, long target)
+{
+    int l = 0;
+    int r = list->size;
+
+    while (l < r) {
+        int m = (l + r) / 2;
+
+        if (list->array[m] >= target) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return l;
+}
+
+// Returns a^n
+long power(long a, int n)
+{
+    long res = 1;
+
+    for (int i = 0; i < n; i++) {
+        res *= a;
+    }
+
+    return res;
+}
+
+void reverse_arr(int *arr, int begin, int end)
+{
+    for (int i = 0, h = (end - begin) / 2; i < h; i++) {
+        int tmp = arr[begin + i];
+        arr[begin + i] = arr[end - 1 - i];
+        arr[end - 1 - i] = tmp;
     }
 }
 
-int next_permutation(int *a, int n)
+int next_permutation(int *perm, int perm_len)
 {
-    int i = n - 2;
+    int i = perm_len - 2;
     for (; i >= 0; i--) {
-        if (a[i] < a[i + 1]) {
+        if (perm[i] < perm[i + 1]) {
             break;
         }
     }
 
     if (i < 0) {
-        reverse(a, 0, n);
+        reverse_arr(perm, 0, perm_len);
         return 0;
     }
 
-    int tmp = a[i];
-    int j = n - 1;
+    int tmp = perm[i];
+    int j = perm_len - 1;
     for (; j > i; j--) {
-        if (a[j] > tmp) {
+        if (perm[j] > tmp) {
             break;
         }
     }
 
-    swap(int, a[i], a[j]);
-    reverse(a, i + 1, n);
+    swap(int, perm[i], perm[j]);
+    reverse_arr(perm, i + 1, perm_len);
 
     return 1;
 }
 
-int bin_search(const int *array, int array_size, int target)
+long polygonal_number(int type, long n)
 {
-    int l = 0;
-    int r = array_size;
-    int m;
-
-    do {
-        m = (l + r) / 2;
-        if (array[m] >= target) {
-            r = m;
-        } else {
-            l = m + 1;
-        }
-    } while (l < r);
-
-    if (l < array_size && array[l] == target) {
-        return l;
-    }
-    return -l - 1;
+    return n * ((type - 2) * n - type + 4) / 2;
 }
 
-int triangle_number(int n)
+long prefix(long num, int prefix_len)
 {
-    return n * (n + 1) / 2;
-}
+    long pow10 = 10;
+    int digits_count = 1;
 
-int square_number(int n)
-{
-    return n * n;
-}
-
-int pentagonal_number(int n)
-{
-    return n * (3 * n - 1) / 2;
-}
-
-int hexagonal_number(int n)
-{
-    return n * (2 * n - 1);
-}
-
-int heptagonal_number(int n)
-{
-    return n * (5 * n - 3) / 2;
-}
-
-int octagonal_number(int n)
-{
-    return n * (3 * n - 2);
-}
-
-int fill_numbers(int *dst, int begin, int end, int (*fn)(int))
-{
-    int n;
-    int number;
-
-    for (n = 1; ; n++) {
-        number = fn(n);
-        if (number >= begin) {
-            break;
-        }
+    while (pow10 <= num) {
+        pow10 *= 10;
+        digits_count++;
     }
 
-    int count = 0;
-
-    for (n++; number < end; n++) {
-        if (number % 100 >= 10) {
-            dst[count++] = number;
-        }
-        number = fn(n);
+    if (prefix_len > digits_count) {
+        return num;
     }
 
-    return count;
+    return num / power(10, digits_count - prefix_len);
 }
 
-int glue(int a, int b)
+long suffix(long num, int suffix_len)
 {
-    return a % 100 * 100 + b / 100;
+    return num % power(10, suffix_len);
 }
 
 int main(void)
 {
-    int numbers[N][LEN];
-    int len[N];
+    LongArrayList polygonal_numbers[NUM_OF_POLYGONAL_TYPES];
+    for (int i = 0; i < NUM_OF_POLYGONAL_TYPES; i++) {
+        LAL_initialize(&polygonal_numbers[i], INITIAL_ARRAYLIST_CAPACITY);
+    }
 
-    len[0] = fill_numbers(numbers[0], BEGIN, END, triangle_number);
-    len[1] = fill_numbers(numbers[1], BEGIN, END, square_number);
-    len[2] = fill_numbers(numbers[2], BEGIN, END, pentagonal_number);
-    len[3] = fill_numbers(numbers[3], BEGIN, END, hexagonal_number);
-    len[4] = fill_numbers(numbers[4], BEGIN, END, heptagonal_number);
-    len[5] = fill_numbers(numbers[5], BEGIN, END, octagonal_number);
+    for (int i = 0; i < NUM_OF_POLYGONAL_TYPES; i++) {
+        const long begin = power(10, NUM_OF_DIGITS - 1);
+        const long end = begin * 10;
 
-    int nums[N / 2];
-    int glues[N / 2];
-    int perm[N / 2];
+        const int polygonal_type = i + 3;
+        int n = 1;
 
-    int res = 0;
+        for (;; n++) {
+            if (polygonal_number(polygonal_type, n) >= begin) {
+                break;
+            }
+        }
 
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
-            if (i == j) {
-                continue;
+        for (;; n++) {
+            const long num = polygonal_number(polygonal_type, n);
+            if (num >= end) {
+                break;
             }
 
-            for (int k = 1, idx = 0; k < N; k++) {
-                if (k != i && k != j) {
-                    perm[idx++] = k;
-                }
-            }
-
-            int l1 = len[0], l2 = len[i], l3 = len[j];
-
-            for (int i1 = 0; i1 < l1; i1++) {
-                nums[0] = numbers[0][i1];
-                for (int i2 = 0; i2 < l2; i2++) {
-                    nums[1] = numbers[i][i2];
-                    for (int i3 = 0; i3 < len[j]; i3++) {
-                        nums[2] = numbers[j][i3];
-                        glues[0] = glue(nums[0], nums[1]);
-                        glues[1] = glue(nums[1], nums[2]);
-                        glues[2] = glue(nums[2], nums[0]);
-
-                        do {
-                            if (
-                                bin_search(numbers[perm[0]], len[perm[0]], glues[0]) >= 0
-                                && bin_search(numbers[perm[1]], len[perm[1]], glues[1]) >= 0
-                                && bin_search(numbers[perm[2]], len[perm[2]], glues[2]) >= 0
-                            ) {
-                                for (int k = 0; k < N / 2; k++) {
-                                    res += nums[k] + glues[k];
-                                }
-                                // printf("%d %d %d %d %d %d\n", nums[0], glues[0], nums[1], glues[1], nums[2], glues[2]);
-                                // printf("0->%d->%d->%d->%d->%d\n", i, j, perm[0], perm[1], perm[2]);
-                                goto Finally;
-                            }
-                        } while (next_permutation(perm, N / 2));
-                    }
-                }
-            }
+            LAL_add(&polygonal_numbers[i], num);
         }
     }
 
+    // Backtracking method
+
+    int perm[NUM_OF_POLYGONAL_TYPES];
+    for (int i = 0; i < NUM_OF_POLYGONAL_TYPES; i++) {
+        perm[i] = i;
+    }
+
+    do {
+        int begin_stk[NUM_OF_POLYGONAL_TYPES];
+        int end_stk[NUM_OF_POLYGONAL_TYPES];
+        int idx_stk[NUM_OF_POLYGONAL_TYPES];
+
+        begin_stk[0] = idx_stk[0] = 0;
+        end_stk[0] = LAL_size(&polygonal_numbers[perm[0]]);
+
+        int stk_size = 1;
+
+        while (stk_size > 0) {
+            const int idx = idx_stk[stk_size - 1];
+
+            if (idx >= end_stk[stk_size - 1]) {
+                stk_size--;
+                if (stk_size > 0) {
+                    idx_stk[stk_size - 1]++;
+                }
+
+                continue;
+            }
+
+            const long suf = suffix(LAL_get(&polygonal_numbers[perm[stk_size - 1]], idx), SUFFIX_LEN);
+            const long lower_bound = suf * power(10, NUM_OF_DIGITS - SUFFIX_LEN);
+            const long upper_bound = (suf + 1) * power(10, NUM_OF_DIGITS - SUFFIX_LEN);
+
+            const LongArrayList *next_list = &polygonal_numbers[perm[stk_size]];
+
+            const int next_begin = LAL_lower_bound(next_list, lower_bound);
+            const int next_end = LAL_lower_bound(next_list, upper_bound);
+
+            if (next_begin >= next_end) {
+                idx_stk[stk_size - 1]++;
+                continue;
+            }
+
+            begin_stk[stk_size] = idx_stk[stk_size] = next_begin;
+            end_stk[stk_size] = next_end;
+            stk_size++;
+
+            if (stk_size == NUM_OF_POLYGONAL_TYPES) {
+                const long first_prefix = prefix(LAL_get(&polygonal_numbers[perm[0]], idx_stk[0]), SUFFIX_LEN);
+
+                for (int last_idx = next_begin; last_idx < next_end; last_idx++) {
+                    const long last_suffix = suffix(LAL_get(next_list, last_idx), SUFFIX_LEN);
+
+                    if (last_suffix == first_prefix) {
+                        long res = 0;
+                        for (int i = 0; i < NUM_OF_POLYGONAL_TYPES; i++) {
+                            res += LAL_get(&polygonal_numbers[perm[i]], idx_stk[i]);
+                        }
+                        printf("%ld\n", res);
+
+                        goto Finally;
+                    }
+                }
+
+                stk_size--;
+                idx_stk[stk_size - 1]++;
+            }
+        }
+    } while (next_permutation(perm, NUM_OF_POLYGONAL_TYPES));
+
 Finally:
-    printf("%d\n", res);
+    // Free memory
+    for (int i = 0; i < NUM_OF_POLYGONAL_TYPES; i++) {
+        LAL_free(&polygonal_numbers[i]);
+    }
 
     return 0;
 }
